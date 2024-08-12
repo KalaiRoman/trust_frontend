30dd82f996ca842de62454b926d16966a39085aa
kalai <kalai@cdp360.com>
2024-08-12 11:58:19 +0530

    new coee push in invoice create

--- src/components/profile/compoents/invoice/Invoice.js
+++ src/components/profile/compoents/invoice/Invoice.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import moment from 'moment';
 import Header from '../../../header';
 import { createInvoice, GetInvoice } from '../../../../services/payment_services/payment_services';
+import { jsPDF } from 'jspdf';
 function Invoice() {
   const {id}=useParams();
   const {token,userdata,removeToken,userdataPayment}=TokenCheck();
@@ -13,24 +14,47 @@ function Invoice() {
 
   const createPdf = (params) => {
 
-    const data={
-        name:userdata?.email,
-        items:userdata?.email,
-        total :100
-    }
-    createInvoice(data)
-      .then(() => GetInvoice({ responseType: 'blob' }))
-      .then((res) => {
-        const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
+    // const data={
+    //     name:userdata?.email,
+    //     items:userdata?.email,
+    //     total :100
+    // }
+    // createInvoice(data)
+    // //   .then(() => GetInvoice({ responseType: 'blob' }))
+    //   .then((res) => {
+    //     console.log(res,"kalaisurya")
+    //     // const pdfBlob = new Blob([res.data.filename], { type: 'application/pdf' });
 
-        const pdfUrl = URL.createObjectURL(pdfBlob);
-        const link = document.createElement('a');
-        link.href = pdfUrl;
-        link.download = 'invoice.pdf';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      });
+    //     // const pdfUrl = URL.createObjectURL(pdfBlob);
+    //     // const link = document.createElement('a');
+    //     // link.href = pdfUrl;
+    //     // link.download = 'invoice.pdf';
+    //     // document.body.appendChild(link);
+    //     // link.click();
+    //     // document.body.removeChild(link);
+    //     const pdfUrl = `/Invoice/${res.data}`;
+    //     const a = document.createElement('a');
+    //     a.href = pdfUrl;
+    //     a.download = 'invoice.pdf';
+    //     a.click();
+    //   });
+
+        const doc = new jsPDF();
+    
+        doc.text("Hello world!", 10, 10);
+    
+        const jsonData = {
+          title: userdata?.email,
+          amount:SinglePayment?.amount,
+          description: "This is an example of generating a PDF in React."
+        };
+    
+        doc.text(jsonData.title, 10, 20);
+        doc.text(jsonData.amount, 10, 30);
+        doc.text(jsonData.description, 10, 40);
+    
+        doc.save("sample.pdf");
+      
   };
   return (
     <div className='mb-5'>
@@ -80,7 +104,7 @@ function Invoice() {
             <p>Thank you for Payment with us!</p>
         </div>
         <div>
-            {/* <button onClick={createPdf}>Donload Invoice</button> */}
+            <button onClick={createPdf}>Donload Invoice</button>
         </div>
     </div>
     
